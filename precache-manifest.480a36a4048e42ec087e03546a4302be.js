a3ba448aeb3af4a13aff7a9d6805c3d28ba3477d
Righen Sawmynaden <righensawmynaden@gmail.com>
2019-11-12 01:12:02 +0400

    Updates

---
+++ precache-manifest.480a36a4048e42ec087e03546a4302be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263597a79c38b5a4585a76cf9c665ceb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a91f2de62c2c0f89635",
+    "url": "/monsters-rolodex/static/css/main.c006db54.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "6a91f2de62c2c0f89635",
+    "url": "/monsters-rolodex/static/js/main.3a1ffb41.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
